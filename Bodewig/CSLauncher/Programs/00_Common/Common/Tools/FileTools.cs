a35fc860041f0506e5cb5c3f1a336be96853fe68
stackprobe <stackprobes@gmail.com>
2018-03-21 00:52:33 +0900

    coffee break

---
+++ Bodewig/CSLauncher/Programs/00_Common/Common/Tools/FileTools.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace Charlotte.Tools
+{
+	public class FileTools
+	{
+		public static void Delete(string path)
+		{
+			if (File.Exists(path))
+			{
+				for (int c = 1; ; c++)
+				{
+					try
+					{
+						File.Delete(path);
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine("ファイル " + path + " の削除に失敗しました。リトライします。" + e.Message);
+					}
+					if (File.Exists(path) == false)
+						break;
+
+					if (10 <= c)
+						throw new Exception("ファイル " + path + " の削除に失敗しました。");
+
+					Thread.Sleep(c * 100);
+					Console.WriteLine("ファイル " + path + " の削除をリトライします。");
+				}
+			}
+			else if (Directory.Exists(path))
+			{
+				for (int c = 1; ; c++)
+				{
+					try
+					{
+						Directory.Delete(path, true);
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine("ディレクトリ " + path + " の削除に失敗しました。リトライします。" + e.Message);
+					}
+					if (Directory.Exists(path) == false)
+						break;
+
+					if (10 <= c)
+						throw new Exception("ディレクトリ " + path + " の削除に失敗しました。");
+
+					Thread.Sleep(c * 100);
+					Console.WriteLine("ディレクトリ " + path + " の削除をリトライします。");
+				}
+			}
+		}
+
+		public static void CreateDir(string dir)
+		{
+			for (int c = 1; ; c++)
+			{
+				try
+				{
+					Directory.CreateDirectory(dir); // dirが存在するときは何もしない。
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine("ディレクトリ " + dir + " の作成に失敗しました。リトライします。" + e.Message);
+				}
+				if (Directory.Exists(dir))
+					break;
+
+				if (10 <= c)
+					throw new Exception("ディレクトリ " + dir + " を作成出来ません。");
+
+				Thread.Sleep(c * 100);
+				Console.WriteLine("ディレクトリ " + dir + " の作成をリトライします。");
+			}
+		}
+
+		public static void CleanupDir(string dir)
+		{
+			foreach (Func<IEnumerable<string>> getPaths in new Func<IEnumerable<string>>[]
+			{
+				() => Directory.EnumerateFiles(dir),
+				() => Directory.EnumerateDirectories(dir),
+			})
+			{
+				foreach (string path in getPaths())
+				{
+					Delete(path);
+				}
+			}
+		}
+	}
+}
---
+++ Bodewig/CSLauncher/Programs/00_Common/Common/Tools/WorkingDir.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Charlotte.Tools
+{
+	public class WorkingDir
+	{
+		private const string ROOT_IDENT = "{0eb7082a-ec41-46bf-beae-6bdadaf4ebbc}"; // UUID v4, only in here
+
+		private static string RootDir = Path.Combine(Environment.GetEnvironmentVariable("TMP"), ROOT_IDENT);
+
+		private string Dir = Path.Combine(RootDir, Guid.NewGuid().ToString("B"));
+
+		public WorkingDir()
+		{
+			using (MSection m = new MSection(ROOT_IDENT)) // RootDir を作成する場合もあるので、排他する。
+			{
+				FileTools.CreateDir(this.Dir);
+			}
+		}
+
+		public string MakePath()
+		{
+			return this.GetPath(Guid.NewGuid().ToString("B"));
+		}
+
+		public string GetPath(string localName)
+		{
+			return Path.Combine(this.Dir, localName);
+		}
+
+		public void Dispose()
+		{
+			FileTools.Delete(this.Dir);
+		}
+
+		public static void Cleanup()
+		{
+			using (MSection m = new MSection(ROOT_IDENT))
+			{
+				FileTools.Delete(RootDir);
+			}
+		}
+	}
+}
