d5238b4df42b83709c5916fd52f73071cf905428
stackprobe <stackprobes@gmail.com>
2018-04-05 22:57:10 +0900

    coffee break

--- Bodewig/CSLauncher/Programs/00_Common/Common/Common.cs
+++ Bodewig/CSLauncher/Programs/00_Common/Common/Common.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 using Charlotte.Tools;
 
@@ -21,6 +23,8 @@ namespace Charlotte
 
 				ArgsReader = new ArgsReader(Environment.GetCommandLineArgs(), 1);
 
+				IntoHomeDir();
+
 				mainFunc(ArgsReader);
 			}
 			catch (Exception e)
@@ -29,6 +33,20 @@ namespace Charlotte
 
 				MessageBox.Show("" + e);
 			}
+		}
+
+		private static void IntoHomeDir()
+		{
+			string self = Assembly.GetEntryAssembly().Location;
+
+			int i = self.ToUpper().LastIndexOf(@"\PROGRAMS\");
+
+			if (i == -1)
+				throw new Exception("ホームディレクトリが見つかりません。");
+
+			string dir = self.Substring(0, i);
+
+			Directory.SetCurrentDirectory(dir);
 		}
 	}
 }
