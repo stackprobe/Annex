be02d5cf9fde3853bcac63fba6da83e8558ff9e2
stackprobe <stackprobes@gmail.com>
2018-08-15 23:55:55 +0900

    tea time

---
+++ ProjectTemplate/HubLauncher/Hub/Hub/Test_0001.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte
+{
+	public class Test_0001
+	{
+		public static void Test01()
+		{
+			Console.WriteLine("Test0001.Test01() OK!");
+		}
+	}
+}
---
+++ ProjectTemplate/HubLauncher/_Readme.txt
@@ -0,0 +1,3 @@
+テスト実行
+
+	run Hub Test_0001 Test01 ;
