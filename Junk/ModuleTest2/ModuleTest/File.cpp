c0931576bbe008ae99c4b78d30e56ea2e89968e1
stackprobe <stackprobes@gmail.com>
2019-01-26 17:36:34 +0900

    daily

--- Junk/ModuleTest2/ModuleTest/File.cpp
+++ Junk/ModuleTest2/ModuleTest/File.cpp
@@ -103,27 +103,6 @@ void unaddCwd(void)
 	setCwd_x(GetCwdStack()->UnaddElement());
 }
 
-// sync > @ My_mkdir
-
-static int My_mkdir(char *dir) // ret: ? é∏îs
-{
-	for(int c = 1; ; c++)
-	{
-		if(_mkdir(dir) == 0) // ? ê¨å˜
-			return 0;
-
-		cout("Failed _mkdir \"%s\", %d-th trial. LastError: %08x\n", dir, c, GetLastError());
-
-		if(10 <= c)
-			break;
-
-		Sleep(100);
-	}
-	return 1;
-}
-
-// < sync
-
 void createFile(char *file)
 {
 	errorCase(m_isEmpty(file));
@@ -132,7 +111,7 @@ void createFile(char *file)
 void createDir(char *dir)
 {
 	errorCase(m_isEmpty(dir));
-	errorCase(My_mkdir(dir)); // ? é∏îs
+	errorCase(_mkdir(dir)); // ? é∏îs
 }
 
 void removeFile(char *file)
