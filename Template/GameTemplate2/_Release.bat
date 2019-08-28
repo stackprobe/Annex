e287f3a2fdc48573b8bceb051560ee1a2f5a1e77
stackprobe <stackprobes@gmail.com>
2019-08-29 07:09:44 +0900

    Gelato

--- Template/GameTemplate2/_Release.bat
+++ Template/GameTemplate2/_Release.bat
@@ -1,6 +1,6 @@
 C:\Factory\Tools\RDMD.exe /RC out
 
-C:\Factory\SubTools\makeDDResourceFile.exe Resource out\Resource.dat
+C:\Factory\SubTools\makeDDResourceFile.exe Resource out\Resource.dat Tools\MaskGZData.exe
 
 C:\Factory\SubTools\CallConfuserCLI.exe GGGGTMPL\GGGGTMPL\bin\Release\GGGGTMPL.exe out\GGGGTMPL.exe
 rem COPY /B GGGGTMPL\GGGGTMPL\bin\Release\GGGGTMPL.exe out
