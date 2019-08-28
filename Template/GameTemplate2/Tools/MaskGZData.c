b4be69eca2db8ee4216272ad681603450b3476e1
stackprobe <stackprobes@gmail.com>
2019-08-29 07:33:38 +0900

    Beat

--- Template/GameTemplate2/Tools/MaskGZData.c
+++ Template/GameTemplate2/Tools/MaskGZData.c
@@ -1,6 +1,6 @@
-#include "C:\Factory\Common\all.h"
+#include "C:\Factory\SubTools\libs\MakeGZData.h"
 
-static void MaskGZData(autoBlock_t *fileData)
+void MaskGZData(autoBlock_t *fileData)
 {
 	if(getSize(fileData))
 	{
@@ -10,26 +10,4 @@ static void MaskGZData(autoBlock_t *fileData)
 		setByte(fileData, 0, 'D');
 		setByte(fileData, 1, 'D');
 	}
-}
-int main(int argc, char **argv)
-{
-	// sync > @ MaskGZData_main
-
-	char *file;
-	autoBlock_t *fileData;
-
-	errorCase(!argIs("MASK-GZ-DATA"));
-
-	file = nextArg();
-	cout("MaskGZData_file: %s\n", file);
-	fileData = readBinary(file);
-	LOGPOS();
-	MaskGZData(fileData);
-	LOGPOS();
-	writeBinary(file, fileData);
-	LOGPOS();
-	releaseAutoBlock(fileData);
-	LOGPOS();
-
-	// < sync
 }
