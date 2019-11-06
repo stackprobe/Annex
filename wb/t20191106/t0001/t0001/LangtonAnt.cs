8fecff745d5a1e98833e14992766846900d2f4cf
stackprobe <stackprobes@gmail.com>
2019-11-07 03:16:54 +0900

    Sword

---
+++ wb/t20191106/t0001/t0001/LangtonAnt.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Charlotte
+{
+	public class LangtonAnt
+	{
+		public class Ant
+		{
+			public Point Pt;
+			public int Direction; // 0-3 == north, west, south, east
+			public Color Color;
+		}
+
+		public int W = 100;
+		public int H = 100;
+		public Color BackColor = Color.Black;
+		public List<Ant> Ants = new List<Ant>();
+		public int ImageCount = 6000;
+		public int Skip = 20;
+
+		public void Perform()
+		{
+			Ant[,] map = new Ant[this.W, this.H];
+
+			for (int count = 0; count < this.ImageCount; count++)
+			{
+				for (int c = 0; c < this.Skip; c++)
+				{
+					foreach (Ant ant in this.Ants)
+					{
+						if (map[ant.Pt.X, ant.Pt.Y] == null)
+						{
+							ant.Direction++;
+							map[ant.Pt.X, ant.Pt.Y] = ant;
+						}
+						else
+						{
+							ant.Direction += 3;
+							map[ant.Pt.X, ant.Pt.Y] = null;
+						}
+						ant.Direction %= 4;
+
+						switch (ant.Direction)
+						{
+							case 0: ant.Pt.Y--; break;
+							case 1: ant.Pt.X--; break;
+							case 2: ant.Pt.Y++; break;
+							case 3: ant.Pt.X++; break;
+						}
+						ant.Pt.X += this.W;
+						ant.Pt.X %= this.W;
+						ant.Pt.Y += this.H;
+						ant.Pt.Y %= this.H;
+					}
+				}
+				DrawImage(map, count);
+			}
+		}
+
+		private void DrawImage(Ant[,] map, int count)
+		{
+			using (Bitmap bmp = new Bitmap(this.W, this.H))
+			{
+				for (int x = 0; x < this.W; x++)
+				{
+					for (int y = 0; y < this.H; y++)
+					{
+						bmp.SetPixel(x, y, map[x, y] == null ? this.BackColor : map[x, y].Color);
+					}
+				}
+				bmp.Save(string.Format(@"C:\temp\{0}.bmp", count));
+			}
+		}
+	}
+}
---
+++ wb/t20191106/t0001/t0001/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using Charlotte.Tools;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Charlotte
+{
+	class Program
+	{
+		public const string APP_IDENT = "{ff19a1a5-f288-476b-805f-bca73f1a3c18}";
+		public const string APP_TITLE = "t0001";
+
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2, APP_IDENT, APP_TITLE);
+
+#if DEBUG
+			//if (ProcMain.CUIError)
+			{
+				Console.WriteLine("Press ENTER.");
+				Console.ReadLine();
+			}
+#endif
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			Test01();
+		}
+
+		private string OutputDir;
+
+		private void Test01()
+		{
+			OutputDir = ExtraTools.MakeFreeDir();
+
+			Test01_Main();
+
+			ProcessTools.Batch(new string[] { "START " + OutputDir });
+		}
+
+		private void Test01_Main()
+		{
+			const int D = 20;
+
+			Test01_a(la =>
+			{
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2, la.H / 2),
+					Direction = 0,
+					Color = Color.White,
+				});
+			});
+
+			Test01_a(la =>
+			{
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 - D, la.H / 2),
+					Direction = 0,
+					Color = Color.Red,
+				});
+
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 + D, la.H / 2),
+					Direction = 0,
+					Color = Color.Blue,
+				});
+			});
+
+			Test01_a(la =>
+			{
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 - D, la.H / 2),
+					Direction = 0,
+					Color = Color.Red,
+				});
+
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 + D, la.H / 2),
+					Direction = 2,
+					Color = Color.DarkCyan,
+				});
+			});
+
+			Test01_a(la =>
+			{
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 - D, la.H / 2 - D),
+					Direction = 0,
+					Color = Color.Red,
+				});
+
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 + D, la.H / 2 + D),
+					Direction = 0,
+					Color = Color.Blue,
+				});
+			});
+
+			Test01_a(la =>
+			{
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 - D, la.H / 2 - D),
+					Direction = 0,
+					Color = Color.Red,
+				});
+
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 + D, la.H / 2 + D),
+					Direction = 2,
+					Color = Color.DarkCyan,
+				});
+			});
+
+			Test01_a(la =>
+			{
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 - D, la.H / 2 - D),
+					Direction = 0,
+					Color = Color.Red,
+				});
+
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 - D, la.H / 2 + D),
+					Direction = 0,
+					Color = Color.Green,
+				});
+
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 + D, la.H / 2 + D),
+					Direction = 0,
+					Color = Color.Yellow,
+				});
+
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 + D, la.H / 2 - D),
+					Direction = 0,
+					Color = Color.Blue,
+				});
+			});
+
+			Test01_a(la =>
+			{
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 - D, la.H / 2 - D),
+					Direction = 0,
+					Color = Color.FromArgb(50, 0, 50),
+				});
+
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 - D, la.H / 2 + D),
+					Direction = 1,
+					Color = Color.FromArgb(100, 0, 100),
+				});
+
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 + D, la.H / 2 + D),
+					Direction = 2,
+					Color = Color.FromArgb(200, 0, 200),
+				});
+
+				la.Ants.Add(new LangtonAnt.Ant()
+				{
+					Pt = new Point(la.W / 2 + D, la.H / 2 - D),
+					Direction = 3,
+					Color = Color.FromArgb(150, 0, 150),
+				});
+			});
+		}
+
+		private int OutputIndex = 0;
+
+		private void Test01_a(Action<LangtonAnt> initLa)
+		{
+			ProcessTools.Batch(new string[] { @"C:\home\bat\onboot\clean_temp.bat" }); // Clear C:\temp
+
+			{
+				LangtonAnt la = new LangtonAnt();
+
+				initLa(la);
+
+				la.Perform();
+			}
+
+			using (WorkingDir wd = new WorkingDir())
+			{
+				string imgDir = wd.MakePath();
+
+				FileTools.CreateDir(imgDir);
+
+				foreach (string file in Directory.GetFiles(@"C:\temp"))
+				{
+					Canvas canvas = new Canvas(file);
+					canvas = ExpandNearest(canvas, 3);
+					canvas.Save(Path.Combine(imgDir, Path.GetFileNameWithoutExtension(file) + ".jpg"), ImageFormat.Jpeg, 100);
+				}
+
+				ProcessTools.Batch(new string[]
+				{
+					string.Format(@"C:\app\ffmpeg-4.1.3-win64-shared\bin\ffmpeg.exe -r 20 -i %%d.jpg {0}\LangtonAnt_{1}.mp4", OutputDir, OutputIndex.ToString("D3")),
+				},
+				imgDir
+				);
+			}
+			OutputIndex++;
+		}
+
+		private Canvas ExpandNearest(Canvas src, int mul)
+		{
+			Canvas dest = new Canvas(src.GetWidth() * mul, src.GetHeight() * mul);
+
+			for (int x = 0; x < dest.GetWidth(); x++)
+			{
+				for (int y = 0; y < dest.GetHeight(); y++)
+				{
+					dest.Set(x, y, src.Get(x / mul, y / mul));
+				}
+			}
+			return dest;
+		}
+	}
+}
