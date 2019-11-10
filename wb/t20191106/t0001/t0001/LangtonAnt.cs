1c25c8141eafae44b524e1bdb32eeba7f93dfde3
stackprobe <stackprobes@gmail.com>
2019-11-10 17:02:33 +0900

    Passion

--- wb/t20191106/t0001/t0001/LangtonAnt.cs
+++ wb/t20191106/t0001/t0001/LangtonAnt.cs
@@ -14,30 +14,38 @@ namespace Charlotte
 			public int Direction; // 0-3 == north, west, south, east
 			public Color Color;
 
+			// <---- prm
+
 			public Point LastPt;
-			public Color? ColorNew;
+			public Color ColorNew;
 		}
 
 		public int W = 100;
 		public int H = 100;
 		public Color BackColor = Color.Black;
 		public List<Ant> Ants = new List<Ant>();
-		public int ImageCount = 600;
-		public int Skip = 100;
+		public int ImageCount = 1200;
+		public int StepPerImage = 100;
+
+		// <---- prm
 
 		public void Perform()
 		{
-			Color?[,] map = new Color?[this.W, this.H];
+			Color[,] map = new Color[this.W, this.H];
+
+			for (int x = 0; x < this.W; x++)
+				for (int y = 0; y < this.H; y++)
+					map[x, y] = this.BackColor;
 
 			for (int count = 0; count < this.ImageCount; count++)
 			{
-				for (int c = 0; c < this.Skip; c++)
+				for (int c = 0; c < this.StepPerImage; c++)
 				{
 					foreach (Ant ant in this.Ants)
 					{
 						ant.LastPt = ant.Pt;
 
-						if (map[ant.Pt.X, ant.Pt.Y] == null)
+						if (map[ant.Pt.X, ant.Pt.Y] == this.BackColor)
 						{
 							ant.Direction++;
 							ant.ColorNew = ant.Color;
@@ -45,7 +53,7 @@ namespace Charlotte
 						else
 						{
 							ant.Direction += 3;
-							ant.ColorNew = null;
+							ant.ColorNew = this.BackColor;
 						}
 						ant.Direction %= 4;
 
@@ -68,17 +76,14 @@ namespace Charlotte
 			}
 		}
 
-		private void DrawImage(Color?[,] map, int count)
+		private void DrawImage(Color[,] map, int count)
 		{
 			using (Bitmap bmp = new Bitmap(this.W, this.H))
 			{
 				for (int x = 0; x < this.W; x++)
-				{
 					for (int y = 0; y < this.H; y++)
-					{
-						bmp.SetPixel(x, y, map[x, y] == null ? BackColor : map[x, y].Value);
-					}
-				}
+						bmp.SetPixel(x, y, map[x, y]);
+
 				bmp.Save(string.Format(@"C:\temp\{0}.bmp", count));
 			}
 		}
