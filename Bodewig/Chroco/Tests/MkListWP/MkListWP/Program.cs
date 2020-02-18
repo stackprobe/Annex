8a07d41416052615e0f0bf0d4d3db20a8ced63d0
stackprobe <stackprobes@gmail.com>
2020-02-19 03:05:03 +0900

    Empress

---
+++ Bodewig/Chroco/Tests/MkListWP/MkListWP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Charlotte.Tools;
+
+namespace Charlotte
+{
+	static class Program
+	{
+		/// <summary>
+		/// アプリケーションのメイン エントリ ポイントです。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			ProcMain.GUIMain(() => new MainWin(), APP_IDENT, APP_TITLE);
+		}
+
+		public const string APP_IDENT = "{fdeb3272-a530-416a-83ef-33966d621300}";
+		public const string APP_TITLE = "MkListWP";
+	}
+}
