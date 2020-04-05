4414f800bcb7d9a08bfc2491ef852b4f9bc35c09
stackprobe <stackprobes@gmail.com>
2020-04-06 03:22:34 +0900

    Pine

---
+++ wb/t20200404/t0002/t0002/Test0001.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte
+{
+	public class Test0001
+	{
+		public void Test01()
+		{
+			for (int c = 1; c <= 1000; c++)
+			{
+				Console.WriteLine(c + "\t" + Test01_f(c));
+			}
+		}
+
+		private int Test01_f(int c)
+		{
+			int l = 0;
+			int r = c;
+
+			while (l + 1 < r)
+			{
+				int m = (l + r) / 2;
+				int p = GetP(m);
+
+				if (p < c)
+					l = m;
+				else
+					r = m;
+			}
+			return r;
+		}
+
+		private int GetP(int t)
+		{
+			int p = 0;
+
+			for (int i = 1; i <= t; i++)
+				p += t / i;
+
+			return p;
+		}
+	}
+}
