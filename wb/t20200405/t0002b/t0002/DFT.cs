765a27ab079002e4edb59569fe8431745cb24b8e
stackprobe <stackprobes@gmail.com>
2020-04-07 02:07:31 +0900

    Lovely

---
+++ wb/t20200405/t0002b/t0002/DFT.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte
+{
+	public static class DFT
+	{
+		public static double Perform(double[][] src, int srcStartPos, int lrIdx, int src_hz, int ret_hz, int size)
+		{
+			double cc = 0.0;
+			double ss = 0.0;
+
+			double mon_hz = Math.PI * 2.0 / src_hz; // これで1Hzになる。
+
+			mon_hz *= ret_hz; // これで(ret_hz)Hzになる。
+
+			for (int offset = 0; offset < size; offset++)
+			{
+				double aa = mon_hz * offset;
+				double vv = src[srcStartPos + offset][lrIdx] * Hamming(offset * 1.0 / (size - 1));
+
+				cc += Math.Cos(aa) * vv;
+				ss += Math.Sin(aa) * vv;
+			}
+			return Math.Sqrt(cc * cc + ss * ss);
+		}
+
+		private static double Hamming(double rate)
+		{
+			return 0.5 - 0.5 * Math.Cos(rate * Math.PI * 2.0);
+		}
+	}
+}
---
+++ wb/t20200405/t0002b/t0002/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using Charlotte.Tools;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Charlotte
+{
+	class Program
+	{
+		public const string APP_IDENT = "{56e576cb-2a4c-44f3-b35a-5d21a03b3ff4}";
+		public const string APP_TITLE = "t0002";
+
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2, APP_IDENT, APP_TITLE);
+
+#if DEBUG
+			//if (ProcMain.CUIError)
+			{
+				Console.WriteLine("Press ENTER.");
+				Console.ReadLine();
+			}
+#endif
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			Test01();
+		}
+
+		private void Test01()
+		{
+			Test01_a(@"C:\temp\jho.wav", @"C:\temp\jho.csv", @"C:\temp\jho.hz", @"C:\temp\jho.mp4");
+			Test01_a(@"C:\temp\ddd.wav", @"C:\temp\ddd.csv", @"C:\temp\ddd.hz", @"C:\temp\ddd.mp4");
+			Test01_a(@"C:\temp\hbn.wav", @"C:\temp\hbn.csv", @"C:\temp\hbn.hz", @"C:\temp\hbn.mp4");
+			Test01_a(@"C:\temp\rlg.wav", @"C:\temp\rlg.csv", @"C:\temp\rlg.hz", @"C:\temp\rlg.mp4");
+		}
+
+		private const int DFT_SIZE = 1000;
+		//private const int DFT_SIZE = 2000;
+		//private const int DFT_SIZE = 3000;
+		//private const int DFT_SIZE = 5000;
+		//private const int FPS = 5;  // 再生に支障
+		//private const int FPS = 10; // 再生に支障
+		private const int FPS = 20;
+
+		private const double SPCT_HI_RATE = 5.0;
+
+#if false
+		private const int BAR_W = 8;
+		private const int BAR_SPAN = 10;
+#elif false
+		private const int BAR_W = 4;
+		private const int BAR_SPAN = 5;
+#else
+		private const int BAR_W = 2;
+		private const int BAR_SPAN = 3;
+#endif
+		private const int IMG_H = 500;
+
+		private static int[] RetHzList = null;
+
+		private void Test01_a(string rWavFile, string rCsvFile, string rHzFile, string wMP4File)
+		{
+			if (RetHzList == null)
+			{
+				List<int> dest = new List<int>();
+
+#if false // 刻みが荒い...
+				for (int hz = 10; hz < 10000; hz = (hz * 11) / 10)
+					dest.Add(hz);
+#elif false
+				for (double hz = 20.0; hz < 10000.0; hz *= 1.05)
+					dest.Add(DoubleTools.ToInt(hz));
+#elif false
+				for (int hz = 20; hz < 1000; hz += 20)
+					dest.Add(hz);
+#else
+				for (int hz = 20; hz <
+					//2000
+					//3000
+					4200 // ピアノの周波数： 27.5 Hz ～ 4186.009 Hz
+					; hz +=
+					//10
+					20
+					)
+					dest.Add(hz);
+#endif
+
+				RetHzList = dest.ToArray();
+			}
+			double[][] wavData;
+
+			using (CsvFileReader reader = new CsvFileReader(rCsvFile))
+			{
+				wavData = reader.ReadToEnd().Select(v => new double[]
+				{
+					(int.Parse(v[0]) / 65535.0 - 0.5) * 2.0,
+					(int.Parse(v[1]) / 65535.0 - 0.5) * 2.0,
+				})
+				.ToArray();
+			}
+			int src_hz = int.Parse(File.ReadAllText(rHzFile).Trim());
+
+			if (src_hz < 1 || IntTools.IMAX < src_hz)
+				throw null; // souteigai !!!
+
+			using (WorkingDir wd = new WorkingDir())
+			{
+				string imgDir = wd.MakePath();
+				string audioFile = wd.GetPath("audio.wav");
+				string outputFile = wd.GetPath("output.mp4");
+
+				FileTools.CreateDir(imgDir);
+				File.Copy(rWavFile, audioFile);
+
+				for (int i = 0; ; i++)
+				{
+					int wavDataPos = (i * src_hz) / FPS;
+
+					if (wavData.Length < wavDataPos + DFT_SIZE)
+						break;
+
+					{
+						int img_w = RetHzList.Length * BAR_SPAN;
+
+						if (img_w % 2 == 1) // 幅(と高さも？)は２の倍数じゃないと ffmpeg は処理出来ない。
+							img_w++;
+
+						Canvas2 canvas = new Canvas2(img_w, IMG_H);
+
+						using (Graphics g = canvas.GetGraphics())
+						{
+							int x = 0;
+
+							foreach (int ret_hz in RetHzList)
+							{
+								double spctL = DFT.Perform(wavData, wavDataPos, 0, src_hz, ret_hz, DFT_SIZE);
+								double spctR = DFT.Perform(wavData, wavDataPos, 1, src_hz, ret_hz, DFT_SIZE);
+								double spct = (spctL + spctR) / 2.0;
+
+								spct *= SPCT_HI_RATE;
+
+								int y = DoubleTools.ToInt(spct);
+
+								g.FillRectangle(Brushes.Orange, x, IMG_H - y, BAR_W, y);
+
+								x += BAR_SPAN;
+							}
+						}
+						canvas.Save(Path.Combine(imgDir, i + ".jpg"), ImageFormat.Jpeg, 100);
+					}
+				}
+
+				ProcessTools.Batch(new string[]
+				{
+					string.Format(@"C:\app\ffmpeg-4.1.3-win64-shared\bin\ffmpeg.exe -r {0} -i %%d.jpg -i ..\{1} ..\{2}", FPS, Path.GetFileName(audioFile), Path.GetFileName(outputFile)),
+				},
+				imgDir
+				);
+
+				FileTools.Delete(wMP4File);
+				File.Move(outputFile, wMP4File);
+			}
+		}
+	}
+}
