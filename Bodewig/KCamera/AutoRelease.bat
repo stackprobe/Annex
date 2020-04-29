cbf1d70488d65b27f6e50b33190bd835c972b563
stackprobe <stackprobes@gmail.com>
2020-04-30 02:27:48 +0900

    Cosmo

--- Bodewig/KCamera/AutoRelease.bat
+++ Bodewig/KCamera/AutoRelease.bat
@@ -1,21 +1,22 @@
-IF NOT EXIST KCamera_is_here.sig GOTO END
 CLS
 rem リリースして qrum します。
 PAUSE
 
 CALL newcsrr
 
+CALL ff
+cx **
+CD /D %~dp0.
+
 CALL qq
 cx **
 
 CALL _Release.bat /-P
 
-MOVE out\KCamera.zip S:\リリース物\.
+MOVE out\* S:\リリース物\.
 
 START "" /B /WAIT /DC:\home\bat syncRev
 
 CALL qrumauto rel
 
 rem **** AUTO RELEASE COMPLETED ****
-
-:END
