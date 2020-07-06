7ce4c8212853f78b0c628c63c7ba34243d7e3bb3
stackprobe <stackprobes@gmail.com>
2020-07-06 16:00:33 +0900

    Egret

---
+++ wb/t20200623_GetBILogarithm/t0001/t0001/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using Charlotte.Tools;
+
+namespace Charlotte
+{
+	class Program
+	{
+		public const string APP_IDENT = "{677e295b-f6c2-447b-bc00-6a6bcd8ff96c}";
+		public const string APP_TITLE = "t0001";
+
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2, APP_IDENT, APP_TITLE);
+
+#if DEBUG
+			//if (ProcMain.CUIError)
+			{
+				Console.WriteLine("Press ENTER.");
+				Console.ReadLine();
+			}
+#endif
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			//new Test0001().Test01();
+			new Test0001().Test02();
+		}
+	}
+}
