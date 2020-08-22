c6ac5a2b594eabb4e13ff3e2378eb7d9851b211f
stackprobe <stackprobes@gmail.com>
2020-08-22 10:10:03 +0900

    Tarvos

---
+++ wb/t20200821_PostShownTest/t0001/t0001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Charlotte.Tools;
+
+namespace Charlotte
+{
+	static class Program
+	{
+		/// <summary>
+		/// アプリケーションのメイン エントリ ポイントです。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			ProcMain.GUIMain(() => new MainWin(), APP_IDENT, APP_TITLE);
+		}
+
+		public const string APP_IDENT = "{fb68573a-e9d6-412d-997c-47883e330089}";
+		public const string APP_TITLE = "t0001";
+	}
+}
